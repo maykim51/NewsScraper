e75e1ceba1314ffe129383912166d24e11f495e8
maykim51 <lapajh@gmail.com>
2019-10-03 16:45:24 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,67 +1,53 @@
-from flask import Flask, jsonify, request, render_template
-
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
-articles = [
+import requests
+from bs4 import BeautifulSoup
 
-]
-count  = 0
-
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-    return 'This is Home!'
+   return 'This is Home!'
 
 @app.route('/mypage')
 def mypage():
-    return render_template('index.html')
+   return render_template('index.html')
 
 
-
+## API 역할을 하는 부분
 @app.route('/post', methods=['POST'])
 def post():
-    global articles            # 이 함수 안에서 나오는 articles 글로벌 변수를 가리킵니다.
-    global count
+   url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
+   comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
 
-    url_receive = request.form['url']          # 클라이언트로부터 url을 받는 부분
-    comment_receive = request.form['comment']  # 클라이언트로부터 comment를 받는 부분
+   headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+   data = requests.get(url_receive, headers=headers)
 
-    count += 1
-    article = {'url':url_receive,'comment':comment_receive, 'index': count} # 받은 걸 딕셔너리로 만들고,
+   soup = BeautifulSoup(data.text, 'html.parser')
 
-    articles[count] = article   # 넣는다
+   og_image = soup.select_one('meta[property="og:image"]')
+   og_title = soup.select_one('meta[property="og:title"]')
+   og_description = soup.select_one('meta[property="og:description"]')
 
-    return jsonify({'result':'success'})
+   url_image = og_image['content']
+   url_title = og_title['content']
+   url_description = og_description['content']
 
+   article = {'url': url_receive, 'comment': comment_receive, 'image': url_image, 'title': url_title, 'desc': url_description}
 
+   db.articles.insert_one(article)
 
-@app.route('/post', methods = ['GET'])
+   return jsonify({'result': 'success'})
+
+@app.route('/post', methods=['GET'])
 def view():
-    print(articles)
-    return jsonify({'result': 'success', 'articles': articles})
+   posts = db.articles.find({},{'_id':0})
+   return jsonify({'result':'success', 'articles':list(posts)})
 
-
-@app.route('/post', methods = ['GET'])
-def view_article():
-    index = request.args.get('index')
-
-
-@app.route('/post', methods=['DELETE'])
-def delete_order():
-    global articles
-    received_index = request.form['index']
-    print("received {}".format(received_index))
-
-    for article in orders:
-        if str(order['index']) == str(received_index):
-            articles.remove(article)
-            return jsonify({'result': 'success', 'orders': articles})
-
-    return jsonify({'result': 'fail'})
-
-
-
-#run app
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug = True)
+   app.run('0.0.0.0',port=5000,debug=True)
